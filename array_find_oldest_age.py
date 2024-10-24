fa25dac317ea793c26a37925bffb64594097a4d9
wynepew <julianwyne12@gmail.com>
2024-10-24 14:31:23 +0800

    started a bit, might change somethings later, testing it first for prior knowledge, will finish later

--- array_find_oldest_age.py
+++ array_find_oldest_age.py
@@ -9,3 +9,30 @@
     #if no
     #print the name and age of the oldest person
 
+citizen = {}
+
+#Loop 1: ask user to input
+while True:
+    #Loop 2: use to retry when input raised an error
+    while True:
+        try:
+            name = input("Input a name: ")
+            age = int(input("Input age: "))
+
+            citizen[name] = {
+                "name" : name,
+                "age" : age
+            }
+            print(citizen[name]["name"])
+            print(citizen[name]["age"])
+
+            retry = input("Do you want to input again: ")
+            break
+        except ValueError:
+            print("Invalid input")
+
+    if retry == "n":
+        break
+    elif retry != "y":
+        print("Invalid")
+
