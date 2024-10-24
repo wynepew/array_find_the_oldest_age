da9fe4b84185d3c6b334eb92ae5646fd8a1a82c0
wynepew <julianwyne12@gmail.com>
2024-10-24 13:56:33 +0800

    done with pseudocode not sure if correct format or correct way

---
+++ array_find_oldest_age.py
@@ -0,0 +1,11 @@
+#ask user 
+    #input name
+    #input age
+    #if invalid 
+    #print error
+#ask user again if they want to input again
+    #if yes
+    #input again
+    #if no
+    #print the name and age of the oldest person
+
